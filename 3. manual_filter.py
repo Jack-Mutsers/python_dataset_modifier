1bcdcda5588fe02a03127e16897863f77ee98705
Jack Mutsers <jmutsers95@hotmail.com>
2022-11-09 10:59:08 +0100

    fixed closing and too many images at once issue

--- 3. manual_filter.py
+++ 3. manual_filter.py
@@ -37,8 +37,9 @@ def loadImages(image_paths):
 
     return loaded_images
 
-def getSelected(tab):
-    global image_array, to_delete
+def getSelected():
+    global image_array, to_delete, tabControl
+    tab = tabControl.index("current")
 
     for i in range(len(image_array[tab])):
         for j in range(len(image_array[tab][i])):
@@ -49,12 +50,13 @@ def getSelected(tab):
 def next_selection():
     global root, loading, tabControl
     t = tabControl.index("current")
-    getSelected(t)
+    getSelected()
 
     if loading == False and len(tabControl.children) > t+1:
         tabControl.select(t+1)
-        # root.destroy()
-        # root = None
+    elif loading == False:
+        root.destroy()
+        root = None
 
 def checkRow(tab_index, x, y):
     global image_array, boxes
@@ -99,74 +101,92 @@ def scanFolder(folder_names, search_dir):
     global image_array, boxes, loading, root, tabControl
 
     for folder_name in folder_names:
-        loading = True
         folder_path = search_dir + folder_name
 
         sub_folder_names = [name for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
         if len(sub_folder_names) > 0:
             scanFolder(sub_folder_names, folder_path)
         
-        root.title(folder_path)
-        root["bg"]='grey'
-
-        tabControl = ttk.Notebook(root, width=colls*20)
-        tabControl.grid(row=1, column=1, columnspan=colls)
-
-        s = ttk.Style()
-        s.theme_use('default')
-        s.configure('TNotebook.Tab', background="grey")
-
-        colspan = math.floor(colls/2)
-        b1 = Button(root, text = "Close", command = close_program, width = 15)
-        b1.grid(row=3, column=1, columnspan=colspan)
-
-        b2 = Button(root, text = "Next", command = next_selection, width = 15)
-        b2.grid(row=3, column=colspan+1, columnspan=colspan)
-        
         image_paths = glob(folder_path+"/*.png")
-        image_path_groups = [image_paths[x:x+total_images] for x in range(0, len(image_paths), total_images)]
+        max_images_per_session = 2500
+        session_groups = [image_paths[x:x+max_images_per_session] for x in range(0, len(image_paths), max_images_per_session)]
 
-        tab_count = 0
-        image_array = []
-        boxes = []
-        for image_path_group in image_path_groups:
-                 
-            images = loadImages(image_path_group)
+        for session_group in session_groups:
+            loading = True
 
-            image_array.append([])
-            for y in range(0, math.ceil(len(images) / colls)):
-                image_array[tab_count].append([])
+            root.title(folder_path)
+            root["bg"]='grey'
 
-            row = 0
-            i = 0
-            for image_index in images:
-                image = images[image_index]
-                image_array[tab_count][row].append(image)
+            tabControl = ttk.Notebook(root, width=colls*65)
+            tabControl.grid(row=1, column=1, columnspan=colls)
 
-                i += 1
-                if len(image_array[tab_count][row]) % colls == 0:
-                    row += 1
-                    i = 0
+            s = ttk.Style()
+            s.theme_use('default')
+            s.configure('TNotebook.Tab', background="grey")
+
+            colspan = math.floor(colls/2)
+            b1 = Button(root, text = "Close", command = close_program, width = 15)
+            b1.grid(row=3, column=1, columnspan=colspan)
+
+            b2 = Button(root, text = "Next", command = next_selection, width = 15)
+            b2.grid(row=3, column=colspan+1, columnspan=colspan)
             
-            tab = tk.Frame(tabControl, background="grey")
-            tabControl.add(tab, text=str(tab_count+1))
+            image_path_groups = [session_group[x:x+total_images] for x in range(0, len(session_group), total_images)]
 
-            boxes.append([])
-            for x in range(len(image_array[tab_count])):
-                image_group = image_array[tab_count][x]
-                boxes[tab_count].append([])
-                for y in range(len(image_group)):
-                    boxes[tab_count][x].append(Checkbutton(tab, variable=image_group[y]["var"], image = image_group[y]["image"], command = lambda x = x: checkRow(tab_count, x, y), background="grey"))
-                    boxes[tab_count][x][y].grid(row=x+1, column=y+1)
+            tab_count = 0
+            image_array = []
+            boxes = []
+            for image_path_group in image_path_groups:
+                images = loadImages(image_path_group)
 
-            tab_count += 1
+                image_array.append([])
+                for y in range(0, math.ceil(len(images) / colls)):
+                    image_array[tab_count].append([])
 
-        loading = False
+                row = 0
+                i = 0
+                for image_index in images:
+                    image = images[image_index]
+                    image_array[tab_count][row].append(image)
 
-        root.mainloop()
-        root = Tk()
+                    i += 1
+                    if len(image_array[tab_count][row]) % colls == 0:
+                        row += 1
+                        i = 0
+                
+                tab = tk.Frame(tabControl, background="grey")
+                tabControl.add(tab, text=str(tab_count+1))
+
+                boxes.append([])
+                for x in range(len(image_array[tab_count])):
+
+                    image_group = image_array[tab_count][x]
+                    boxes[tab_count].append([])
+                    for y in range(len(image_group)):
+                        var = image_group[y]["var"]
+                        image = image_group[y]["image"]
+                        command = lambda x = x: checkRow(tab_count, x, y)
+                        boxes[tab_count][x].append(Checkbutton(tab, variable = var, image = image, command = command, background="grey"))
+                        boxes[tab_count][x][y].grid(row=x+1, column=y+1)
+
+                tab_count += 1
+
+            loading = False
+
+            root.mainloop()
+
+            # unset variables
+            tabControl = None
+            root = None
+
+            # reset tkinter window
+            root = Tk()
             
-        remove_selected_files()
+            # delete selected images
+            remove_selected_files()
+
+
+
 
 for character in labelNames:
     character_index = labelNames.index(character)
